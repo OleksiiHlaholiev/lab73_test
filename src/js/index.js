f7fecc0d1970f457dc0d13450f1e5c67dfd62437
glalex <oleksii.hlaholiev@gmail.com>
2018-02-28 23:38:11 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
 $(function () {
-    $('#menu-btn').on('click', function () {
+    var header = $('#header'),
+        menuBtn = $('#menu-btn'),
+        mainMenu = $('#main-menu');
+
+
+    $(menuBtn).on('click', function () {
         $(this).toggleClass('active');
+        $(header).toggleClass('active');
+        $(mainMenu).fadeToggle(400);
     })
 });
