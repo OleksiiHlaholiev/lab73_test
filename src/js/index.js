d6825c014c7a05968a3bc37c6e7d3217dfe378f4
glalex <oleksii.hlaholiev@gmail.com>
2018-03-01 21:27:29 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -30,6 +30,7 @@ $(function () {
         $(imgTextGuru).addClass('transparent').removeClass('animated zoomIn');
         $(userBtns).addClass('transparent').removeClass('animated fadeInUp');
     }
+
     function pageAnimateState() {
         setTimeout(function () {
             $(headerFullCont).removeClass('transparent').addClass('animated slideInDown');
@@ -82,6 +83,7 @@ $(function () {
             $(menuItems).addClass('transparent').removeClass('animated fadeInLeft');
         }, 400);
     }
+
     function menuAnimateState() {
         menuStateFlag = 1;
         $(mainMenu).fadeIn(400);
@@ -113,4 +115,37 @@ $(function () {
         (menuStateFlag === 0) ? menuAnimateState() : menuInitState();
     });
 
+});
+
+// parallax effect with GSAP
+$(function () {
+    var $parallaxContainer = $(".macbook-cont"); // our container
+    var $parallaxItems = $parallaxContainer.find(".parallax");  //elements
+    var fixer = 0.0008;		//experiment with the value
+
+    $(document).on("mousemove", function (event) {
+
+        var pageX = event.pageX - ($parallaxContainer.width() * 0.5);  //get the mouseX - negative on left, positive on right
+        var pageY = event.pageY - ($parallaxContainer.height() * 0.5); //same here, get the y. use console.log(pageY) to see the values
+
+        //here we move each item
+        $parallaxItems.each(function () {
+
+            var item = $(this);
+            var speedX = item.data("speed-x");
+            var speedY = item.data("speed-y");
+
+            /*TweenLite.to(item, 0.5, {
+							x: (item.position().left + pageX * speedX)*fixer,    //calculate the new X based on mouse position * speed
+							y: (item.position().top + pageY * speedY)*fixer
+						});*/
+
+            //or use set - not so smooth, but better performance
+            TweenLite.set(item, {
+                x: (item.position().left + pageX * speedX) * fixer,
+                y: (item.position().top + pageY * speedY) * fixer
+            });
+
+        });
+    });
 });
